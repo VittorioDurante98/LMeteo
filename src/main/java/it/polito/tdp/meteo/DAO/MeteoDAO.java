69f16a9f6d355fb57dfac7d348f7e79e17423868
vitto <vitto@192.168.1.129>
2020-07-13 15:40:57 +0200

    completo

--- src/main/java/it/polito/tdp/meteo/DAO/MeteoDAO.java
+++ src/main/java/it/polito/tdp/meteo/DAO/MeteoDAO.java
@@ -1,13 +1,14 @@
 package it.polito.tdp.meteo.DAO;
 
 import java.sql.Connection;
+
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.Date;
+import java.util.LinkedList;
 import java.util.List;
-
+import it.polito.tdp.meteo.model.Citta;
 import it.polito.tdp.meteo.model.Rilevamento;
 
 public class MeteoDAO {
@@ -40,15 +41,15 @@ public class MeteoDAO {
 		}
 	}
 
-	public List<Rilevamento> getAllRilevamentiLocalitaMese(int mese, String localita) {
+	public List<Rilevamento> getAllRilevamentiLocalitaMese(int mese, Citta c) {
 		final String sql = "SELECT ?, Data, Umidita FROM situazione WHERE Data=? and localita=?" ;
 		List<Rilevamento> rilevamentiMese = new ArrayList<Rilevamento>();
 
 		try {
 			Connection conn = ConnectDB.getConnection();
 			PreparedStatement st = conn.prepareStatement(sql);
-			st.setString(1, localita);
-			st.setString(3, localita);
+			st.setString(1, c.getNome());
+			st.setString(3, c.getNome());
 			Rilevamento r;
 			
 			for(int i= 0; i<32; i++) {
@@ -56,7 +57,7 @@ public class MeteoDAO {
 				st.setString(2, s);
 				ResultSet rs= st.executeQuery();
 				while (rs.next()) {
-					r = new Rilevamento(localita, rs.getDate("Data"), rs.getInt("Umidita"));
+					r = new Rilevamento(c.getNome(), rs.getDate("Data"), rs.getInt("Umidita"));
 					rilevamentiMese.add(r);
 				}
 			}
@@ -72,10 +73,10 @@ public class MeteoDAO {
 		
 	}
 
-	public double getAvgRilevamentiLocalitaMese(int mese, String localita) {
+	public double getAvgRilevamentiLocalitaMese(int mese, Citta c) {
 		double media =0.0;
 		int somma=0;
-		List<Rilevamento> rilevamenti = new ArrayList<>(this.getAllRilevamentiLocalitaMese(mese, localita));
+		List<Rilevamento> rilevamenti = new ArrayList<>(this.getAllRilevamentiLocalitaMese(mese, c));
 		for(Rilevamento r: rilevamenti) {
 			somma+= r.getUmidita();
 		}
@@ -84,4 +85,30 @@ public class MeteoDAO {
 	}
 
 
+	public List<Citta> getAllCitta() {
+		List<Citta> allCitta = new LinkedList<>();
+		final String sql = "SELECT Localita FROM situazione";
+
+		try {
+			Connection conn = ConnectDB.getConnection();
+			PreparedStatement st = conn.prepareStatement(sql);
+
+			ResultSet rs = st.executeQuery();
+
+			while (rs.next()) {
+
+				Citta c = new Citta(rs.getString("Localita"));
+				allCitta.add(c);
+			}
+
+			conn.close();
+			return allCitta;
+
+		} catch (SQLException e) {
+
+			e.printStackTrace();
+			throw new RuntimeException(e);
+		}
+	}
+	
 }
--- src/main/java/it/polito/tdp/meteo/model/Model.java
+++ src/main/java/it/polito/tdp/meteo/model/Model.java
@@ -1,7 +1,10 @@
 package it.polito.tdp.meteo.model;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 
 import it.polito.tdp.meteo.DAO.MeteoDAO;
 
@@ -13,25 +16,63 @@ public class Model {
 	private final static int NUMERO_GIORNI_TOTALI = 15;
 
 	private MeteoDAO mdao= new MeteoDAO();
+	private int livello;
+	private Map<Date,String> spostamenti = new TreeMap<>();
+	private int numGiorniTo;
+	private int numGiorniMi;
+	private int numGiorniGe;
+	private int sommaTo=0;
+	private int sommaGe=0;
+	private int sommaMi=0;
+	List<Rilevamento> parziale= null;
 	
 	public Model() {
-
+		this.livello=0;
+		this.sommaTo=0;
+		this.sommaMi=0;
+		this.sommaGe=0;
+		this.numGiorniGe=0;
+		this.numGiorniMi=0;
+		this.numGiorniTo=0;
 	}
 
 	// of course you can change the String output with what you think works best
 	public String getUmiditaMedia(int mese) {
-		double mt= mdao.getAvgRilevamentiLocalitaMese(mese, "Torino");
-		double mg= mdao.getAvgRilevamentiLocalitaMese(mese, "Genova");
-		double mm= mdao.getAvgRilevamentiLocalitaMese(mese, "Milano");
+		Citta c1 = new Citta("Torino");
+		Citta c2 = new Citta("Milano");
+		Citta c3 = new Citta("Genova");
+		
+		double mt= mdao.getAvgRilevamentiLocalitaMese(mese, c1);
+		double mg= mdao.getAvgRilevamentiLocalitaMese(mese, c2);
+		double mm= mdao.getAvgRilevamentiLocalitaMese(mese, c3);
 		
 		String s= "Torino: "+mt+"\nGenova: "+mg+"\nMilano: "+mm+"\n";
 		return s;
 	}
 	
 	// of course you can change the String output with what you think works best
-	public String trovaSequenza(int mese) {
-		return "TODO!";
+	public Map<Date,String> trovaSequenza(int mese) {
+		List<Citta> leCitta=  new ArrayList<>();
+		for(Citta c: leCitta)
+			c.setRilevamenti(mdao.getAllRilevamentiLocalitaMese(mese, c));
+		
+	
+		cerca(parziale,livello);
+		
+		
+		return spostamenti;
 	}
 	
+	public void cerca(List<Rilevamento> parziale, int livello) {
+		
+		
+		if(livello==15)
+			return;
+		
+		for(int i=0; i<15; i++) {
+			
+		}
+		
+	}
 
 }
